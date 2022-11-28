83a994fcbe68d815ca461246c40f9fe248bedcf3
joris-landaret <joris.landaret@laplateforme.io>
2022-11-28 15:20:08 +0100

    création de la page connexion brut

---
+++ connexion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="css/connexion.css">
+</head>
+
+<body>
+    <?php include('header.php') ?>
+    <main>
+
+        <div>
+            <h1>Connecte toi vite !!!</h1>
+            <form action="">
+                <label for="">Login</label>
+                <Input></Input>
+
+                <label for="">Password</label>
+                <Input></Input>
+
+                <input type="submit" value="vite !!!">
+            </form>
+        </div>
+
+    </main>
+    <?php include('footer.php') ?>
+</body>
+
+</html>
