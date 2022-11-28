1df9df866bb5576e467f7e0938dccc3d38eaf12e
joris-landaret <joris.landaret@laplateforme.io>
2022-11-28 13:53:14 +0100

    création d'un header brut

--- header.php
+++ header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insolite rainbow</title>
-    <link rel="stylesheet" href="css/header.css">
-</head>
-<body>
     <header>
         <div>
             <img src="img/images.jpg" alt="">
@@ -24,5 +14,3 @@
         </div>
 
     </header>
-</body>
-</html>
